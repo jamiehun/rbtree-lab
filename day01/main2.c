95c65b90201c8ede18610ee6e36112581af0c697
Ubuntu <ubuntu@ip-172-31-34-35.ap-southeast-1.compute.internal>
2022-10-25 08:05:10 +0000

    rbtree_insert_1

---
+++ day01/main2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main(){
+    int my_int = -10;
+
+    printf("%08x\n", my_int);
+}
--- src/rbtree.c
+++ src/rbtree.c
@@ -4,7 +4,13 @@
 
 rbtree *new_rbtree(void) {
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
+  node_t *new_node = (node_t *)calloc(1, sizeof(node_t));
+
   // TODO: initialize struct if needed
+  new_node->color = RBTREE_BLACK;
+  p->nil = new_node;
+  p->root = p->nil;
+
   return p;
 }
 
@@ -13,8 +19,41 @@ void delete_rbtree(rbtree *t) {
   free(t);
 }
 
-node_t *rbtree_insert(rbtree *t, const key_t key) {
+node_t *rbtree_insert(rbtree *t, const key_t key) { // 트리와 insert할 값을 넣음
   // TODO: implement insert
+  // CLRS pseudo code 참고
+  node_t *y = (node_t *)calloc(1, sizeof(node_t));
+  node_t *x = (node_t *)calloc(1, sizeof(node_t));
+  node_t *new_node = (node_t *)calloc(1, sizeof(node_t)); // z에 대한 새로운 노드
+  new_node->key = key // z에 대한 key값 설정
+  
+  y = t->nil; // 구조체 안에 있는 멤버는 투포인터로 안찍어도 되나?
+  x = t->root; 
+
+  while ( x != t->nil )
+  {
+    y = x;
+    
+    if (key < x->key)
+      x = x->left;
+    else x = x->right;
+
+  }
+
+  // line 8: z.p = y
+  new_node->parent = y;
+
+  // line 9 ~ line 13
+  if (y == t->nil)       // 처음부터 nil으로 빈 트리를 의미
+    t->root = new_node;
+  else if (new_node->key < y->key)
+
+
+
+
+
+
+
   return t->root;
 }
 
